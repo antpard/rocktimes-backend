ea6b09f391218f4130d33345c1e85b6b21bfab21
Antonio Pardo <apardo@spluca.org>
2024-10-30 14:45:15 +0100

    use environment variables for address and port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes import user_routes, auth_routes
+import os
 
 
 app = FastAPI()
@@ -10,4 +11,5 @@ app.include_router(auth_routes.router, prefix="/api/v1/auth", tags=["auth"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host=os.environ["HOSTNAME"], port=os.environ["PORT"])
+  
--- routes/event_routes.py
+++ routes/event_routes.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
-from controllers import auth_controller
+from controllers import event_controller
 
 
 router = APIRouter()
 
 @router.get("/")
-def get_all_users():
-    return user_controller.get_all_users()
+def get_all_events():
+    return event_controller.get_all_events()
 
 @router.post("/")
-def create_user(user: user_controller.UserCreate):
-    return user_controller.create_user(user)
+def create_post(event: event_controller.EventCreate):
+    return event_controller.create_event(event)
